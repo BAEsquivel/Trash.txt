3b69d2fb1e61b8b25cc9a5d070a618de5bc660ed
Rebecca A. Esquivel <42479478+BAEsquivel@users.noreply.github.com>
2019-10-15 23:18:30 -0700

    Update webpack.config.js

--- examples/ssr/webpack.config.js
+++ examples/ssr/webpack.config.js
@@ -1,18 +1,1 @@
-/* eslint-env node */
-module.exports = {
-  entry: './client/index.js',
-  output: {
-    filename: 'index.js',
-    path: `${__dirname}/build`,
-    publicPath: '/'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: ['babel-loader']
-      }
-    ]
-  }
-}
+
