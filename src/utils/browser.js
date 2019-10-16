a176ab71f9265dedc569e9bd39cb72ad9633d651
Rebecca A. Esquivel <42479478+BAEsquivel@users.noreply.github.com>
2019-10-16 02:12:09 -0700

    Update browser.js

--- src/utils/browser.js
+++ src/utils/browser.js
@@ -1,8 +1,1 @@
-const ua =
-  typeof window !== 'undefined' &&
-  window.navigator &&
-  window.navigator.userAgent
 
-export const windowsPhone = ua && /IEMobile|Windows Phone/i.test(ua)
-export const ios = ua && /iPhone|iPad|iPod/i.test(ua) && !windowsPhone
-export const android = ua && /Android/i.test(ua) && !windowsPhone
