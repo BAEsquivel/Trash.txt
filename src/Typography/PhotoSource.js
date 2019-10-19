3a3f8d6f1382ab3ce42d6e1f57835ed3da5a0f23
Rebecca A. Esquivel <42479478+BAEsquivel@users.noreply.github.com>
2019-10-19 08:55:09 -0700

    Delete PhotoSource.js

--- src/Typography/PhotoSource.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import Typography from './Typography'
-
-const PhotoSource = props => (
-  <Typography tagName="span" {...props} type="photoSource" />
-)
-
-export default PhotoSource
