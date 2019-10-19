66643a30ab8d705f0dd1c87bbb57a8c7a032e880
Rebecca A. Esquivel <42479478+BAEsquivel@users.noreply.github.com>
2019-10-19 08:54:38 -0700

    Delete Epigraph.js

--- src/Typography/Epigraph.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import Typography from './Typography'
-
-const Epigraph = props => (
-  <Typography tagName="div" {...props} type="epigraph" />
-)
-
-export default Epigraph
