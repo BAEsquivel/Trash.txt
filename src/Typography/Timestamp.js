9baca13090a0065c88f0590c313044aeebd55837
Rebecca A. Esquivel <42479478+BAEsquivel@users.noreply.github.com>
2019-10-19 08:55:01 -0700

    Delete Timestamp.js

--- src/Typography/Timestamp.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import Typography from './Typography'
-
-const Timestamp = props => (
-  <Typography tagName="time" {...props} type="timestamp" />
-)
-
-export default Timestamp
